02580f9452013e28edb126f610e1620b3e8e4889
Emily Jensen <emje6419@colorado.edu>
2020-12-06 15:21:02 -0700

    add level 2

--- make_leveled_files.py
+++ make_leveled_files.py
@@ -10,3 +10,12 @@ schools_unique_sorted.name = 'schoolID'
 schools_unique_sorted.to_csv('level3.csv',index=False,header=True)
 print('There are {} unique schools in the dataset'.format(len(schools_unique_sorted)))
 ## There are 658 unique schools in the dataset
+
+# make level 2 (student) file
+# this file contains schoolID, studentID, and achievement scores at beginning (prior) and end (EOC) of the school year
+students = df[['user_account','school_number_eoc','dev_scale_score_nq_c','scale_score_nq_c']]
+students.columns = ['studentID','schoolID','prior','EOC']
+students_sorted = students.sort_values(['schoolID','studentID'])
+students_sorted.to_csv('level2.csv',index=False,header=True)
+print('There are {} unique students in the dataset'.format(len(students_sorted)))
+## There are 86916 unique students in the dataset
