abaa749cbd46a1dc30b387dedd1f368103e0f80e
Emily Jensen <emje6419@colorado.edu>
2020-12-06 16:19:13 -0700

    add level 1

--- make_leveled_files.py
+++ make_leveled_files.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # make level 3 (school) file
 # this file just contains schoolID, since we do not have any other school-level predictors
@@ -19,3 +20,19 @@ students_sorted = students.sort_values(['schoolID','studentID'])
 students_sorted.to_csv('level2.csv',index=False,header=True)
 print('There are {} unique students in the dataset'.format(len(students_sorted)))
 ## There are 86916 unique students in the dataset
+
+# make level 1 (survey) file
+# this file contains schoolID, studentID, time of survey, survey answer, and some other survey info that might be of interest later
+surveys = pd.read_csv('survey-answers-matched-scaled.csv')
+surveys_filtered = surveys[['user_id','survey_id','timestamp_created','survey_question','survey_valence','survey_answer_unipolar']]
+surveys_filtered.columns = ['studentID','surveyID','surveyTime','surveyEmotion','emotionValence','surveyResponse']
+surveys_answered = surveys_filtered.loc[surveys_filtered['surveyResponse'] > 0]
+surveys_merged = surveys_answered.merge(students_sorted, how='inner', on='studentID')
+surveys_merged['surveyTime'] = pd.to_datetime(surveys_merged['surveyTime'], format='%Y-%m-%d %H:%M:%S')
+first_survey = min(surveys_merged['surveyTime'])
+surveys_merged['monthStart'] = (surveys_merged['surveyTime'].dt.year - first_survey.year) * 12 + (surveys_merged['surveyTime'].dt.month - first_survey.month)
+surveys_merged['monthEnd'] = surveys_merged['monthStart'] - max(surveys_merged['monthStart'])
+surveys_sorted = surveys_merged.sort_values(['schoolID','studentID','surveyTime'])
+surveys_sorted.to_csv('level3.csv',index=False,header=True)
+print('There are {} total surveys'.format(len(surveys_sorted)))
+## There are 89164 total surveys
