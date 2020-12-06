851ab51210be6b520bf26c3f61481875f031a3ac
Emily Jensen <emje6419@colorado.edu>
2020-12-06 16:55:06 -0700

    keep same students and schools in each dataset

--- make_leveled_files.py
+++ make_leveled_files.py
@@ -8,18 +8,12 @@ schools = df['school_number_eoc']
 schools_unique = schools.drop_duplicates()
 schools_unique_sorted = schools_unique.sort_values()
 schools_unique_sorted.name = 'school'
-schools_unique_sorted.to_csv('level3.csv',index=False,header=True)
-print('There are {} unique schools in the dataset'.format(len(schools_unique_sorted)))
-## There are 658 unique schools in the dataset
 
 # make level 2 (student) file
 # this file contains schoolID, studentID, and achievement scores at beginning (prior) and end (EOC) of the school year
 students = df[['user_account','school_number_eoc','dev_scale_score_nq_c','scale_score_nq_c']]
 students.columns = ['student','school','prior','EOC']
 students_sorted = students.sort_values(['school','student'])
-students_sorted.to_csv('level2.csv',index=False,header=True)
-print('There are {} unique students in the dataset'.format(len(students_sorted)))
-## There are 86916 unique students in the dataset
 
 # make level 1 (survey) file
 # this file contains schoolID, studentID, time of survey, survey answer, and some other survey info that might be of interest later
@@ -33,6 +27,20 @@ first_survey = min(surveys_merged['time'])
 surveys_merged['monthS'] = (surveys_merged['time'].dt.year - first_survey.year) * 12 + (surveys_merged['time'].dt.month - first_survey.month)
 surveys_merged['monthE'] = surveys_merged['monthS'] - max(surveys_merged['monthS'])
 surveys_sorted = surveys_merged.sort_values(['school','student','time'])
+
+# make sure there are the same number of distinct students and schools in each file
+# some of these were dropped when matching students with schools and surveys with students
+students_sorted = students_sorted.loc[students_sorted['student'].isin(surveys_sorted['student'])]
+students_sorted = students_sorted.loc[students_sorted['school'].isin(surveys_sorted['school'])]
+schools_unique_sorted = schools_unique_sorted.loc[schools_unique_sorted.isin(surveys_sorted['school'])]
+
+# save everything to csv
 surveys_sorted.to_csv('level1.csv',index=False,header=True)
 print('There are {} total surveys'.format(len(surveys_sorted)))
 ## There are 89164 total surveys
+students_sorted.to_csv('level2.csv',index=False,header=True)
+print('There are {} unique students in the dataset'.format(len(students_sorted)))
+## There are 42053 unique students in the dataset
+schools_unique_sorted.to_csv('level3.csv',index=False,header=True)
+print('There are {} unique schools in the dataset'.format(len(schools_unique_sorted)))
+## There are 590 unique schools in the dataset
